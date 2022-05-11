a9910713b997b67777926fbe42ec7e152445429a
sertamon <stenyaev1997@gmail.com>
2022-05-11 13:18:17 +0300

    Добавление карусели фотографий

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-let buttonAdded = document.getElementById('buttonAdded');
-let buttonDelete = document.getElementById('buttonDelete');
-let uploadFile = document.getElementById('uploadFile');
-
+const buttonAdded = document.getElementById('buttonAdded');
+const buttonDelete = document.getElementById('buttonDelete');
+const uploadFile = document.getElementById('uploadFile');
+const photo = document.querySelectorAll('.user_photo_added');
+const arrowLeft = document.querySelector('.arrow_photo_left');
+const arrowRight = document.querySelector('.arrow_photo_right');
+let i = 0
 
 
 
@@ -31,4 +34,21 @@ let showFile = function (input) {
     if(buttonAdded.classList.contains('hidden') == false){
         document.getElementById('conclusionFile').innerText = fileName;
     }
+}
+
+arrowLeft.onclick = function(){
+    photo[i].style.display = 'none';
+    i = i - 1;
+    if(i < 0){
+        i = photo.length - 1;
+    }
+    photo[i].style.display = 'block';
+}
+arrowRight.onclick = function(){
+    photo[i].style.display = 'none';
+    i = i + 1;
+    if(i > photo.length - 1){
+        i = 0;
+    }
+    photo[i].style.display = 'block';
 }
